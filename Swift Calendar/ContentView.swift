4a01cb4dfa45eff3d2b42449218a1972d6cfabc8
Nizami <nizami@about-me.app>
2023-01-19 12:44:28 +0500

    Calendar UI

--- Swift Calendar/ContentView.swift
+++ Swift Calendar/ContentView.swift
@@ -15,14 +15,35 @@ struct ContentView: View {
         sortDescriptors: [NSSortDescriptor(keyPath: \DayEntity.date, ascending: true)],
         animation: .default)
     private var days: FetchedResults<DayEntity>
+    private let daysOfWeek = ["M", "T", "W", "T", "F", "S", "S"]
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(days) { day in
-                    Text(day.date!.formatted())
+            VStack {
+                HStack {
+                    ForEach(daysOfWeek, id: \.self) { dayOfWeek in
+                        Text(dayOfWeek)
+                            .fontWeight(.black)
+                            .foregroundColor(.orange)
+                            .frame(maxWidth: .infinity)
+                    }
                 }
+                LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 7)) {
+                    ForEach(days) { day in
+                        Text(day.date!.formatted(.dateTime.day()))
+                            .bold()
+                            .foregroundColor(day.didStudy ? .orange : .secondary)
+                            .frame(maxWidth: .infinity, minHeight: 40)
+                            .background(
+                                Circle()
+                                    .foregroundColor(.orange.opacity(day.didStudy ? 0.3 : 0))
+                            )
+                    }
+                }
+                Spacer()
             }
+            .navigationTitle(Date().formatted(.dateTime.month(.wide)))
+            .padding()
         }
     }
 }
--- Swift Calendar/Persistence.swift
+++ Swift Calendar/Persistence.swift
@@ -10,31 +10,11 @@ import CoreData
 struct PersistenceController {
     static let shared = PersistenceController()
 
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newDay = DayEntity(context: viewContext)
-            newDay.date = Date()
-            newDay.didStudy = Bool.random()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
-
     let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "Swift_Calendar")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
+
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
 
